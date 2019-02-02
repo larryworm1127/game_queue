78c61b122667c7c91f8a98aec0f586ecb58fd9e9
larryworm1127 <shilerong@gmail.com>
2019-02-02 13:32:03 -0500

    added validators

--- game_queue/forms.py
+++ game_queue/forms.py
@@ -1,7 +1,17 @@
 from flask_wtf import Form
 from wtforms import StringField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
 
 
+# Forms
 class UsernameForm(Form):
     username = StringField('Username', validators=[DataRequired()])
+
+
+# Validators
+class Unique(object):
+    def __init__(self, message: str) -> None:
+        self.message = message
+
+    def __call__(self, form, field) -> None:
+        pass
