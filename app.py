56263797bf47b59c61c13d41ba7ef3740ba967bf
larryworm1127 <shilerong@gmail.com>
2019-02-02 11:47:08 -0500

    added forms

--- app.py
+++ app.py
@@ -1,11 +1,22 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
+
+from .forms import UsernameForm
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    form = UsernameForm()
+    if form.validate_on_submit():
+        return redirect(url_for('queue'))
+
     return render_template('index.html')
+
+
+@app.route('/queue')
+def queue():
+    return 'queue'
 
 
 if __name__ == '__main__':
